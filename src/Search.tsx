06984e7e6e2c5bb2016287657b38dfb17b12141e
Bryndye <hugocluzel17@hotmail.fr>
2024-11-06 14:53:58 +0100

    search bar

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,28 +1,103 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Button, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Image } from 'react-native-elements';
+import { useColorScheme } from 'react-native';
+import { getThemeStyles, getStyles } from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-function SearchScreen({navigation}: any) {
+const SearchScreen: React.FC = () => {
+  const [query, setQuery] = useState('');
+  interface Movie {
+    id: number;
+    poster_path: string;
+    original_title: string;
+  }
+
+  const [results, setResults] = useState<Movie[]>([]);
+  const isDarkMode = useColorScheme() === 'dark';
+  const stylesTheme = getThemeStyles(isDarkMode);
+  const styles_ = getStyles();
+  const navigation = useNavigation<any>();
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${query}`);
+      const data = await response.json();
+      setResults(data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Home')}
-      />
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
+    <View style={[styles.container, stylesTheme.backgroundColor]}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:'center' }}>
+        <Icon name="search" size={25} color='#F2C94C' style={{ marginRight: 10, height:40, alignSelf:'center' }} />
+        <TextInput
+          style={[styles.searchBar, { flex: 1 }]}
+          placeholder="Search for a movie..."
+          value={query}
+          onChangeText={setQuery}
+          onSubmitEditing={fetchMovies}
+        />
+      </View>
+      {results.length === 0 && <Text style={[styles.titleMovie, stylesTheme.textColor]}>No results found</Text>}
+      <FlatList
+        data={results}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => navigation.navigate('Home', { screen: 'MovieDetails', params: { item: item } })}
+          >
+            <View style={{ height: '75%' }}>
+              <Image
+                source={{ uri: `https://image.tmdb.org/t/p/w400/${item.poster_path}` }}
+                style={styles_.imageMovie}
+              />
+            </View>
+            <Text style={[styles.titleMovie, stylesTheme.textColor]}>{item.original_title}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id.toString()}
+        horizontal={false}
+        showsVerticalScrollIndicator={false}
+        numColumns={3}
+        style={{ flex: 1 }}
       />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  searchBar: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    width: '100%',
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: 10,
+    gap: 10,
+    marginBottom: 10,
+    marginRight: '5%',
+    width: '30%',
+    height: 200,
+  },
+  titleMovie: {
+    width: '100%',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+});
 
 export default SearchScreen;
